6c4c8935cdfcd6c8c68132a2e785c178c1888e92
huyhuanhg <huyhuanhg@gmail.com>
2022-01-06 17:31:33 +0700

    api test method

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function index(Request $request)
+    {
+        return response()->json([
+            'method' => $request->method()
+        ]);
+    }
+    public function store(Request $request)
+    {
+        return response()->json([
+            'method' => $request->method()
+        ]);
+    }
+    public function update(Request $request, $id)
+    {
+        return response()->json([
+            'method' => $request->method(),
+            'product_id' => $id
+        ]);
+    }
+    public function destroy(Request $request, $id)
+    {
+        return response()->json([
+            'method' => $request->method(),
+            'product_id' => $id
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Shopify\Clients\Rest;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,5 +16,51 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return "Hello API";
+    try {
+        $client = new Rest("your-development-store.myshopify.com", "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");
+        $response = $client->post(
+            "metafields",
+            [
+                "metafield" => [
+                    "namespace" => "global",
+                    "key" => "product_request_icons",
+                    "value" => json_encode(['name' => "test"]),
+                    "type" => "json"
+                ]
+            ]
+        );
+        echo "<pre>";
+        print_r($response);
+        echo "</pre>";
+        return response()->json($response);
+    } catch (\Exception $exception) {
+        return response()->json(['msg' => $exception->getMessage()]);
+    }
+});
+
+Route::get('/get', function () {
+    try {
+        $client = new Rest("your-development-store.myshopify.com", "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");
+        $response = $client->get(
+            "metafields"
+        );
+        return response()->json($response);
+    } catch (\Exception $exception) {
+        return response()->json(['msg' => $exception->getMessage()]);
+    }
+});
+
+Route::post('/product/{id}/request', function (Request $request, $id) {
+    return response()->json([
+        'product_id' => $id,
+        'icon_id' => $request->iconId
+    ]);
+});
+
+Route::resource('/test-method', 'App\Http\Controllers\TestController')->except(['create', 'show', 'edit']);
+
+
+Route::fallback(function () {
+    return response()->json([
+        'message' => 'Page Not Found. If error persists, contact info@website.com'], 404);
 });
