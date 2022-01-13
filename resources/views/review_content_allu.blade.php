5ebf39f7488cd3a083f4b28f3ad7b91494a06a7a
huyhuanhg <huyhuanhg@gmail.com>
2022-01-13 17:30:58 +0700

    blade

---
+++ resources/views/review_content_allu.blade.php
@@ -0,0 +1,191 @@
+<div class="revico-review-comment-row-wrapper" data-product-first-view="false" style="">
+    <div class="revico-review-comment-row revico_yellow">
+        <div class="revico-review-comment-detail">
+            <a href="https://allu-official.com/shop/g/gA0891294/">
+                <div class="revico-review-comment-detail--product-name">
+                    <div class="revico-review-comment-detail--product-name-image">
+                        <img src="https://allu-official.com/img/goods/L/A0891294.JPG">
+                    </div>
+                    <p>ヴィトン ファストレーンライン デニム スニーカー 7 メンズ ブルー モノグラム</p>
+                    <span>詳細を見る<img src="https://show.revico.jp/img/product_link.png"></span>
+                </div>
+            </a>
+            <div class="revico-star-block">
+                <div class="revico-star-block--rate"><p
+                        class="revico-starrate starability-result revico-star-value-5"></p></div>
+                <div class="revico-review-comment-detail--date">2022.1.13</div>
+            </div>
+
+            <div class="revico-review-comment-detail--reviewer-wrapper">
+                <div>
+                    <div class="revico-star-block--review-title no_image">シューズ</div>
+                    <ul class="revico-review-comment-head--variation" style="display: none;">
+                        <li>
+                            <span class="revico-review-comment-head--variation-1">色：</span>
+                            <span class="revico-review-comment-head--variation-2">サイズ：</span>
+                        </li>
+                    </ul>
+                    <ul class="revico-review-comment-head--evaluationanswer"></ul>
+
+
+                    <div class="revico-review-comment-head">
+                        <div class="revico-review-comment-head--image"><img
+                                src="https://show.revico.jp/img/anonymous_icon.gif"></div>
+                        <div class="revico-review-comment-head--reviewer-name">ヨッシー</div>
+                    </div>
+                    <div class="revico-review-comment-head--reviewer-purchased">購入確認済み</div>
+                    <ul class="revico-review-comment-head--reviewer-evaluationanswer"></ul>
+                </div>
+            </div>
+
+            <div class="revico-review-comment-detail--comment">
+                <div class="revico-review-comment-detail--content">想像と商品評価より全く状態は良かったです、安心して買い物出来ました。<br></div>
+                <div class="revico-review-comment-detail--more"><span>続きを読む</span></div>
+            </div>
+            <div class="revico-review-comment-detail--images">
+                <ul></ul>
+            </div>
+
+            <div class="revico-review-comment-detail--sns-reference">
+                <div class="revico-review-comment-detail--sns">
+                    <a href="https://twitter.com/intent/tweet?url=https%3A%2F%2Fallu-official.com%2Fshop%2Fg%2FgA0891294%2F%3Frevico&amp;text=%E3%83%A8%E3%83%83%E3%82%B7%E3%83%BC%E3%81%95%E3%82%93%E3%81%AB%E3%82%88%E3%82%8B%E3%80%8C%E3%83%B4%E3%82%A3%E3%83%88%E3%83%B3%20%E3%83%95%E3%82%A1%E3%82%B9%E3%83%88%E3%83%AC%E3%83%BC%E3%83%B3%E3%83%A9%E3%82%A4%E3%83%B3%20%E3%83%87%E3%83%8B%E3%83%A0%20%E3%82%B9%E3%83%8B%E3%83%BC%E3%82%AB%E3%83%BC%207%20%E3%83%A1%E3%83%B3%E3%82%BA%20%E3%83%96%E3%83%AB%E3%83%BC%20%E3%83%A2%E3%83%8E%E3%82%B0%E3%83%A9%E3%83%A0%E3%80%8D%E3%81%AE%E3%83%AC%E3%83%93%E3%83%A5%E3%83%BC%0A%E8%A9%95%E4%BE%A1%3A%20%E2%98%85%E2%98%85%E2%98%85%E2%98%85%E2%98%85%0A%E3%80%8C%E6%83%B3%E5%83%8F%E3%81%A8%E5%95%86%E5%93%81%E8%A9%95%E4%BE%A1%E3%82%88%E3%82%8A%E5%85%A8%E3%81%8F%E7%8A%B6%E6%85%8B%E3%81%AF%E8%89%AF%E3%81%8B%E3%81%A3%E3%81%9F%E3%81%A7%E3%81%99%E3%80%81%E5%AE%89%E5%BF%83%E3%81%97%E3%81%A6%E8%B2%B7%E3%81%84%E7%89%A9%E5%87%BA%E2%80%A6%E3%80%8D%0A&amp;hashtags=ReviCo"
+                       target="_blank"><img class="revico-review-comment-detail--sns-twitter"
+                                            src="https://show.revico.jp/img/twi.png"></a>
+                    <a href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fallu-official.com%2Fshop%2Fg%2FgA0891294%2F%3Frevico"
+                       target="_blank"><img class="revico-review-comment-detail--sns-facebook"
+                                            src="https://show.revico.jp/img/fb.png"></a>
+                    <a href="http://line.me/R/msg/text/?%E3%83%A8%E3%83%83%E3%82%B7%E3%83%BC%E3%81%95%E3%82%93%E3%81%AB%E3%82%88%E3%82%8B%E3%80%8C%E3%83%B4%E3%82%A3%E3%83%88%E3%83%B3%20%E3%83%95%E3%82%A1%E3%82%B9%E3%83%88%E3%83%AC%E3%83%BC%E3%83%B3%E3%83%A9%E3%82%A4%E3%83%B3%20%E3%83%87%E3%83%8B%E3%83%A0%20%E3%82%B9%E3%83%8B%E3%83%BC%E3%82%AB%E3%83%BC%207%20%E3%83%A1%E3%83%B3%E3%82%BA%20%E3%83%96%E3%83%AB%E3%83%BC%20%E3%83%A2%E3%83%8E%E3%82%B0%E3%83%A9%E3%83%A0%E3%80%8D%E3%81%AE%E3%83%AC%E3%83%93%E3%83%A5%E3%83%BC%0A%E8%A9%95%E4%BE%A1%3A%20%E2%98%85%E2%98%85%E2%98%85%E2%98%85%E2%98%85%0A%E3%80%8C%E6%83%B3%E5%83%8F%E3%81%A8%E5%95%86%E5%93%81%E8%A9%95%E4%BE%A1%E3%82%88%E3%82%8A%E5%85%A8%E3%81%8F%E7%8A%B6%E6%85%8B%E3%81%AF%E8%89%AF%E3%81%8B%E3%81%A3%E3%81%9F%E3%81%A7%E3%81%99%E3%80%81%E5%AE%89%E5%BF%83%E3%81%97%E3%81%A6%E8%B2%B7%E3%81%84%E7%89%A9%E5%87%BA%E2%80%A6%E3%80%8D%0A%E8%A9%B3%E7%B4%B0%E3%81%AF%E3%81%93%E3%81%A1%E3%82%89%E3%81%A7%E3%83%81%E3%82%A7%E3%83%83%E3%82%AF%EF%BC%81%0Ahttps%3A%2F%2Fallu-official.com%2Fshop%2Fg%2FgA0891294%2F%3Frevico%0A"
+                       target="_blank"><img class="revico-review-comment-detail--sns-line"
+                                            src="https://show.revico.jp/img/line.png"></a>
+                </div>
+                <div class="revico-review-comment-detail--reference-block">
+                    <div class="revico-review-comment-detail--reference">参考になった
+                        <span class="revico-review-comment-detail--reference-count">0</span>
+                    </div>
+                    <p style="display:none;">ご参考にしていただき、ありがとうございました。</p>
+                </div>
+            </div>
+            <div class="store-comment-wrapper">
+                <div class="store-comment-tri-wrapper">
+                    <div class="store-comment">
+                        <p class="store-comment--title">店舗からの回答
+                            <span class="store-comment--title-date">2022.1.13</span>
+                        </p>
+
+                    </div>
+                    <div class="store-comment-tri"></div>
+                </div>
+                <div class="store-comment-img">
+                    <img src="https://show.revico.jp/img/shop_icon.png">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="revico-review-comment-row-wrapper" data-product-first-view="false" style="">
+    <div class="revico-review-comment-row revico_yellow">
+        <div class="revico-review-comment-detail">
+            <a href="https://allu-official.com/shop/g/gXO015847/">
+                <div class="revico-review-comment-detail--product-name">
+                    <div class="revico-review-comment-detail--product-name-image">
+                        <img src="https://allu-official.com/img/goods/L/XO015847.JPG">
+                    </div>
+                    <p>ヴィトン エピ アルマBB M58706</p>
+                    <span>詳細を見る<img src="https://show.revico.jp/img/product_link.png"></span>
+                </div>
+            </a>
+            <div class="revico-star-block">
+                <div class="revico-star-block--rate"><p
+                        class="revico-starrate starability-result revico-star-value-5"></p></div>
+                <div class="revico-review-comment-detail--date">2022.1.12</div>
+            </div>
+
+            <div class="revico-review-comment-detail--reviewer-wrapper">
+                <div>
+                    <div class="revico-star-block--review-title no_image">アルマbb</div>
+                    <ul class="revico-review-comment-head--variation" style="display: none;">
+                        <li>
+                            <span class="revico-review-comment-head--variation-1">色：</span>
+                            <span class="revico-review-comment-head--variation-2">サイズ：</span>
+                        </li>
+                    </ul>
+                    <ul class="revico-review-comment-head--evaluationanswer"></ul>
+
+
+                    <div class="revico-review-comment-head">
+                        <div class="revico-review-comment-head--image"><img
+                                src="https://show.revico.jp/img/anonymous_icon.gif"></div>
+                        <div class="revico-review-comment-head--reviewer-name">ちょーみ</div>
+                    </div>
+                    <div class="revico-review-comment-head--reviewer-purchased">購入確認済み</div>
+                    <ul class="revico-review-comment-head--reviewer-evaluationanswer">
+                        <li data-revico-reviewer-evaluationanswer-id="00000000-0000-0000-0000-000000000001"><span
+                                class="revico-review-comment-head--reviewer-evaluationanswer-title">年代:</span><span
+                                class="revico-review-comment-head--reviewer-evaluationanswer-choice">40代</span></li>
+                        <li data-revico-reviewer-evaluationanswer-id="00000000-0000-0000-0000-000000000002"><span
+                                class="revico-review-comment-head--reviewer-evaluationanswer-title">性別:</span><span
+                                class="revico-review-comment-head--reviewer-evaluationanswer-choice">女性</span></li>
+                        <li data-revico-reviewer-evaluationanswer-id="00000000-0000-0000-0000-000000000003"><span
+                                class="revico-review-comment-head--reviewer-evaluationanswer-title">身長:</span><span
+                                class="revico-review-comment-head--reviewer-evaluationanswer-choice">161～165cm</span>
+                        </li>
+                        <li data-revico-reviewer-evaluationanswer-id="00000000-0000-0000-0000-000000000004"><span
+                                class="revico-review-comment-head--reviewer-evaluationanswer-title">体型:</span><span
+                                class="revico-review-comment-head--reviewer-evaluationanswer-choice">大柄</span></li>
+                        <li data-revico-reviewer-evaluationanswer-id="00000000-0000-0000-0000-000000000005"><span
+                                class="revico-review-comment-head--reviewer-evaluationanswer-title">靴のサイズ:</span><span
+                                class="revico-review-comment-head--reviewer-evaluationanswer-choice">25cm</span></li>
+                        <li data-revico-reviewer-evaluationanswer-id="0f4831bf-0825-4474-86df-0746567f0405"><span
+                                class="revico-review-comment-head--reviewer-evaluationanswer-title">リユース（中古）品の購入経験:</span><span
+                                class="revico-review-comment-head--reviewer-evaluationanswer-choice">5回以上</span></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="revico-review-comment-detail--comment">
+                <div class="revico-review-comment-detail--content">Aランクで購入。<br>新品同様に綺麗なお品で、大満足でした。<br></div>
+                <div class="revico-review-comment-detail--more"><span>続きを読む</span></div>
+            </div>
+            <div class="revico-review-comment-detail--images">
+                <ul></ul>
+            </div>
+
+            <div class="revico-review-comment-detail--sns-reference">
+                <div class="revico-review-comment-detail--sns">
+                    <a href="https://twitter.com/intent/tweet?url=https%3A%2F%2Fallu-official.com%2Fshop%2Fg%2FgXO015847%2F%3Frevico&amp;text=%E3%81%A1%E3%82%87%E3%83%BC%E3%81%BF%E3%81%95%E3%82%93%E3%81%AB%E3%82%88%E3%82%8B%E3%80%8C%E3%83%B4%E3%82%A3%E3%83%88%E3%83%B3%20%E3%82%A8%E3%83%94%20%E3%82%A2%E3%83%AB%E3%83%9EBB%20M58706%E3%80%8D%E3%81%AE%E3%83%AC%E3%83%93%E3%83%A5%E3%83%BC%0A%E8%A9%95%E4%BE%A1%3A%20%E2%98%85%E2%98%85%E2%98%85%E2%98%85%E2%98%85%0A%E3%80%8CA%E3%83%A9%E3%83%B3%E3%82%AF%E3%81%A7%E8%B3%BC%E5%85%A5%E3%80%82%0A%E6%96%B0%E5%93%81%E5%90%8C%E6%A7%98%E3%81%AB%E7%B6%BA%E9%BA%97%E3%81%AA%E3%81%8A%E5%93%81%E3%81%A7%E3%80%81%E5%A4%A7%E6%BA%80%E8%B6%B3%E3%81%A7%E3%81%97%E3%81%9F%E3%80%82%0A%E3%80%8D&amp;hashtags=ReviCo"
+                       target="_blank"><img class="revico-review-comment-detail--sns-twitter"
+                                            src="https://show.revico.jp/img/twi.png"></a>
+                    <a href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fallu-official.com%2Fshop%2Fg%2FgXO015847%2F%3Frevico"
+                       target="_blank"><img class="revico-review-comment-detail--sns-facebook"
+                                            src="https://show.revico.jp/img/fb.png"></a>
+                    <a href="http://line.me/R/msg/text/?%E3%81%A1%E3%82%87%E3%83%BC%E3%81%BF%E3%81%95%E3%82%93%E3%81%AB%E3%82%88%E3%82%8B%E3%80%8C%E3%83%B4%E3%82%A3%E3%83%88%E3%83%B3%20%E3%82%A8%E3%83%94%20%E3%82%A2%E3%83%AB%E3%83%9EBB%20M58706%E3%80%8D%E3%81%AE%E3%83%AC%E3%83%93%E3%83%A5%E3%83%BC%0A%E8%A9%95%E4%BE%A1%3A%20%E2%98%85%E2%98%85%E2%98%85%E2%98%85%E2%98%85%0A%E3%80%8CA%E3%83%A9%E3%83%B3%E3%82%AF%E3%81%A7%E8%B3%BC%E5%85%A5%E3%80%82%0A%E6%96%B0%E5%93%81%E5%90%8C%E6%A7%98%E3%81%AB%E7%B6%BA%E9%BA%97%E3%81%AA%E3%81%8A%E5%93%81%E3%81%A7%E3%80%81%E5%A4%A7%E6%BA%80%E8%B6%B3%E3%81%A7%E3%81%97%E3%81%9F%E3%80%82%0A%E3%80%8D%E8%A9%B3%E7%B4%B0%E3%81%AF%E3%81%93%E3%81%A1%E3%82%89%E3%81%A7%E3%83%81%E3%82%A7%E3%83%83%E3%82%AF%EF%BC%81%0Ahttps%3A%2F%2Fallu-official.com%2Fshop%2Fg%2FgXO015847%2F%3Frevico%0A"
+                       target="_blank"><img class="revico-review-comment-detail--sns-line"
+                                            src="https://show.revico.jp/img/line.png"></a>
+                </div>
+                <div class="revico-review-comment-detail--reference-block">
+                    <div class="revico-review-comment-detail--reference">参考になった
+                        <span class="revico-review-comment-detail--reference-count">0</span>
+                    </div>
+                    <p style="display:none;">ご参考にしていただき、ありがとうございました。</p>
+                </div>
+            </div>
+            <div class="store-comment-wrapper">
+                <div class="store-comment-tri-wrapper">
+                    <div class="store-comment">
+                        <p class="store-comment--title">店舗からの回答
+                            <span class="store-comment--title-date">2022.1.12</span>
+                        </p>
+                        <p class="store-comment--content">
+
+                        </p>
+                    </div>
+                    <div class="store-comment-tri"></div>
+                </div>
+                <div class="store-comment-img">
+                    <img src="https://show.revico.jp/img/shop_icon.png">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Shopify\Clients\Rest;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,7 +16,6 @@ use Shopify\Clients\Rest;
 |
 */
 
-use Session;
 
 Route::get('/', function () {
     try {
@@ -147,6 +147,217 @@ Route::get('/product/{product_id}/rate', function ($product_id) {
 Route::get('/product/{product_id}/visit', function ($product_id) {
     return response()->json([
         "visit_count" => rand(1, 100)
+    ]);
+});
+
+//instagram carousel
+Route::get('/instagram', function () {
+    return response()->json([
+        'type' => 'all',
+        'images' => [
+            "https://video.visumo.jp/image/e3051808-1f0a-44ab-aeda-2be31536319a/carousel/e50b1b0e-0735-4b93-a1ce-b9e797911915-large.jpg",
+            "https://video.visumo.jp/image/e3051808-1f0a-44ab-aeda-2be31536319a/e50c030d-0f15-4918-a175-e5c73ecdb2b2-large.jpg",
+            "https://video.visumo.jp/image/e3051808-1f0a-44ab-aeda-2be31536319a/carousel/e40b0509-1006-4e0d-9a81-4fedbac8fdfa-large.jpg",
+            "https://video.visumo.jp/image/e3051808-1f0a-44ab-aeda-2be31536319a/carousel/e50c0b0e-1109-4189-8ae3-843e68a6532d-large.jpg",
+            "https://video.visumo.jp/image/e3051808-1f0a-44ab-aeda-2be31536319a/e50c0e10-0e02-44cd-99ba-28c5ed7f747a-large.jpg",
+            'https://video.visumo.jp/image/e3051808-1f0a-44ab-aeda-2be31536319a/carousel/e50c0b0e-1109-4477-b766-e3f1dd8963f1-large.jpg',
+            'https://video.visumo.jp/image/e3051808-1f0a-44ab-aeda-2be31536319a/e50c1f10-382d-47e5-b895-fafab7445383-large.jpg',
+            'https://video.visumo.jp/image/e3051808-1f0a-44ab-aeda-2be31536319a/e50c1c10-3910-4302-9925-b4f78873856b-large.jpg',
+            'https://video.visumo.jp/image/e3051808-1f0a-44ab-aeda-2be31536319a/e50c1b10-3a0c-4a02-add5-94bcd15d106b-large.jpg',
+            'https://video.visumo.jp/image/e3051808-1f0a-44ab-aeda-2be31536319a/e50c1910-3a21-4da2-8c0c-0ca1f6233a61-large.jpg',
+            'https://video.visumo.jp/image/e3051808-1f0a-44ab-aeda-2be31536319a/e50c1910-3a20-4fa5-8e16-3b749144cc9d-large.jpg',
+            'https://video.visumo.jp/image/e3051808-1f0a-44ab-aeda-2be31536319a/e50c1905-022a-4890-92d1-1e12cc8452a0-large.jpg',
+            'https://video.visumo.jp/image/e3051808-1f0a-44ab-aeda-2be31536319a/carousel/e5090a0b-1f04-4313-89c2-9a544808ba6e-large.jpg',
+            'https://video.visumo.jp/image/e3051808-1f0a-44ab-aeda-2be31536319a/e50c0109-0222-4b9a-a29b-0fff7f78f762-large.jpg',
+            'https://video.visumo.jp/image/e3051808-1f0a-44ab-aeda-2be31536319a/carousel/e5010e14-2f28-44fa-b773-ef82d1137d06-large.jpg',
+            'https://video.visumo.jp/image/e3051808-1f0a-44ab-aeda-2be31536319a/e50a1910-340c-4753-892e-ccbd1c4bb643-large.jpg',
+            'https://video.visumo.jp/image/e3051808-1f0a-44ab-aeda-2be31536319a/e50b0505-022b-4cd8-81ec-c07d8bdcc04d-large.jpg',
+            'https://video.visumo.jp/image/e3051808-1f0a-44ab-aeda-2be31536319a/e50a1610-3421-4de9-9878-b7276d154538-large.jpg',
+            'https://video.visumo.jp/image/e3051808-1f0a-44ab-aeda-2be31536319a/e50a1610-3421-4f2d-8b62-85bf92c2c924-large.jpg',
+            'https://video.visumo.jp/image/e3051808-1f0a-44ab-aeda-2be31536319a/e50a1604-3303-4ca8-b265-b9234aa44823-large.jpg',
+            'https://video.visumo.jp/image/e3051808-1f0a-44ab-aeda-2be31536319a/e50a1310-3516-43a7-9095-319c6f1854f0-large.jpg',
+            'https://video.visumo.jp/image/e3051808-1f0a-44ab-aeda-2be31536319a/e50a0f10-382b-4430-bb8b-016b2894e9d7-large.jpg',
+            "https://video.visumo.jp/image/e3051808-1f0a-44ab-aeda-2be31536319a/e50a0c10-3407-4f92-8352-c9937fd267ff-large.jpg",
+            "https://video.visumo.jp/image/e3051808-1f0a-44ab-aeda-2be31536319a/e50a0b10-3433-4173-8af9-a2fea474aaeb-large.jpg",
+            "https://video.visumo.jp/image/e3051808-1f0a-44ab-aeda-2be31536319a/e5081210-3b15-4f74-8ecf-4a3ac3c68510-large.jpg",
+            "https://video.visumo.jp/image/e3051808-1f0a-44ab-aeda-2be31536319a/e5081010-3a17-4b7a-a2dd-7103f0a5d6a9-large.jpg",
+            "https://video.visumo.jp/image/e3051808-1f0a-44ab-aeda-2be31536319a/e5080d11-0b33-409d-b5af-cc1c89e732c4-large.jpg",
+            "https://video.visumo.jp/image/e3051808-1f0a-44ab-aeda-2be31536319a/e5080910-3a38-4c63-a993-e28c5c8bb730-large.jpg",
+            "https://video.visumo.jp/image/e3051808-1f0a-44ab-aeda-2be31536319a/e5080611-0104-4eb2-863c-34262979d602-large.jpg",
+        ]
+    ]);
+});
+
+//get instagram type
+
+Route::get('/instagram/type', function () {
+    return response()->json([
+        [
+            'type' => 'バッグ',
+            'type_id' => 1
+        ],
+        [
+            'type' => '財布/小物',
+            'type_id' => 2
+        ],
+        [
+            'type' => 'アパレル',
+            'type_id' => 3
+        ],
+        [
+            'type' => 'アクセサリー',
+            'type_id' => 4
+        ],
+        [
+            'type' => 'ファッション雑貨',
+            'type_id' => 5
+        ],
+        [
+            'type' => '時計',
+            'type_id' => 6
+        ],
+        [
+            'type' => 'ネクタイ',
+            'type_id' => 7
+        ],
+        [
+            'type' => 'その他',
+            'type_id' => 8
+        ],
+        [
+            'type' => 'エルメス',
+            'type_id' => 9
+        ]
+    ]);
+});
+
+
+//user Review store
+Route::get('/user-review', function () {
+    return view('review_content_allu');
+//    return response()->json([
+//        [
+//            'id' => 1,
+//            'user_name' => "ヨッシー",
+//            "user_avatar" => "https://show.revico.jp/img/anonymous_icon.gif",
+//            "review_title" => "シューズ",
+//            "review_rate" => 5,
+//            "review_content" => "",
+//            "transaction_status" => "購入確認済み",
+//            "created_at" => "2022.1.13",
+//            "product" => [
+//                "title" => "ヴィトン ファストレーンライン デニム スニーカー 7 メンズ ブルー モノグラム",
+//                "image" => "https://allu-official.com/img/goods/L/A0891294.JPG"
+//            ],
+//            "store_answer" => [
+//                "content" => 'この度は、弊社商品をご注文いただき誠にありがとうございます。<br><br>状態の良い商品をお客様にお届けすることができ、私も大変嬉しく思います。今後も安心してご利用いただけるよう努めて参りますので、またのご利用を楽しみにお待ちしております♪',
+//                "created_at" => "2022.1.13"
+//            ]
+//        ],
+//        [
+//            'id' => 2,
+//            'user_name' => "ちょーみ",
+//            "user_avatar" => "https://show.revico.jp/img/anonymous_icon.gif",
+//            "review_title" => "アルマbb",
+//            "review_rate" => 5,
+//            "review_content" => "Aランクで購入。<br>新品同様に綺麗なお品で、大満足でした。<br>",
+//            "transaction_status" => "購入確認済み",
+//            "created_at" => "2022.1.12",
+//            "product" => [
+//                "title" => "ヴィトン エピ アルマBB M58706",
+//                "image" => "https://allu-official.com/img/goods/L/XO015847.JPG"
+//            ],
+//            "store_answer" => [
+//                "content" => "この度は、ALLUをご利用いただき誠にありがとうございます。<br><br>大満足とのお言葉を聞くことができ、スタッフ一同大変嬉しく思います♪お色味も可愛らしい素敵な商品ですので、ぜひご愛用頂ければ幸いです。<br><br>またのご利用を楽しみにお待ちしております。",
+//                "created_at" => "2022.1.12"
+//            ]
+//        ],
+//    ]);
+});
+
+//user review filter list
+Route::get('/review-filter', function () {
+    return response()->json([
+        [
+            'id' => 1,
+            'title' => '商品'
+        ],
+        [
+            'id' => 2,
+            'title' => '状態'
+        ],
+        [
+            'id' => 3,
+            'title' => '大満足'
+        ],
+        [
+            'id' => 4,
+            'title' => '梱包'
+        ],
+        [
+            'id' => 5,
+            'title' => '対応'
+        ],
+        [
+            'id' => 6,
+            'title' => '満足'
+        ],
+        [
+            'id' => 7,
+            'title' => '買い物'
+        ],
+        [
+            'id' => 8,
+            'title' => '新品'
+        ],
+        [
+            'id' => 9,
+            'title' => 'もの'
+        ],
+        [
+            'id' => 10,
+            'title' => '機会'
+        ],
+        [
+            'id' => 11,
+            'title' => '発送'
+        ],
+        [
+            'id' => 12,
+            'title' => 'こちら'
+        ],
+        [
+            'id' => 13,
+            'title' => '美品'
+        ],
+        [
+            'id' => 14,
+            'title' => '商品の状態'
+        ],
+        [
+            'id' => 15,
+            'title' => '品物'
+        ],
+        [
+            'id' => 16,
+            'title' => 'サイズ'
+        ],
+        [
+            'id' => 17,
+            'title' => '購入'
+        ],
+        [
+            'id' => 18,
+            'title' => '使用感'
+        ],
+        [
+            'id' => 19,
+            'title' => 'お品'
+        ],
+        [
+            'id' => 20,
+            'title' => '今回'
+        ]
     ]);
 });
 
